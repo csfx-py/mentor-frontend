bc15a77642d3e5feecec1933e8161a03db38b245
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-07 09:44:28 +0530

    init and router

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
