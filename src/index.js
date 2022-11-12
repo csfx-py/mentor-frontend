c2d9ca82175a3c98306603dbd74b53ee4cccd9fd
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-12 16:10:40 +0530

    wrapped provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { UserProvider } from "./Contexts/UserContext";
+import { SnackbarProvider } from "notistack";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider maxSnack={3}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </SnackbarProvider>
   </React.StrictMode>
 );
 
