225a519799dfd0e984f1983405c7b043fcac6b38
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-12 16:11:26 +0530

    routes to feed and auth

--- src/App.js
+++ src/App.js
@@ -1,47 +1,68 @@
 import { Typography } from "@mui/material";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useSnackbar } from "notistack";
+import { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes
+} from "react-router-dom";
 import "./App.css";
 import Err404 from "./Assets/404.jpeg";
+import { FeedProvider } from "./Contexts/FeedContext";
+import { UserContext } from "./Contexts/UserContext";
 import Auth from "./Routes/Auth";
 import Feed from "./Routes/Feed";
 
 function App() {
+  const { user, userData } = useContext(UserContext);
+  // eslint-disable-next-line no-unused-vars
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
   return (
     <div className="App">
       <Router>
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route
-            path="*"
-            element={
-              <div
-                style={{
-                  height: "100vh",
-                  background:
-                    "linear-gradient(135deg, #FFEB3B 0%, #ffffff 100%)",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  flexDirection: "column",
-                }}
-              >
-                <img
-                  src={Err404}
-                  alt="404"
+        <FeedProvider userData={userData}>
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route
+              path="/auth"
+              element={user ? <Navigate to="/feed" /> : <Auth />}
+            />
+            <Route
+              path="/feed"
+              element={user ? <Feed /> : <Navigate to="/auth" />}
+            />
+            <Route
+              path="*"
+              element={
+                <div
                   style={{
-                    maxHeight: "100vh",
-                    minHeight: "40vh",
+                    height: "100vh",
+                    background:
+                      "linear-gradient(135deg, #FFEB3B 0%, #ffffff 100%)",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "column",
                   }}
-                />
-                <Typography variant="h4" align="center">
-                  404 - Page Not Found
-                </Typography>
-              </div>
-            }
-          />
-        </Routes>
+                >
+                  <img
+                    src={Err404}
+                    alt="404"
+                    style={{
+                      maxHeight: "100vh",
+                      minHeight: "40vh",
+                    }}
+                  />
+                  <Typography variant="h4" align="center">
+                    404 - Page Not Found
+                  </Typography>
+                </div>
+              }
+            />
+          </Routes>
+        </FeedProvider>
       </Router>
     </div>
   );
