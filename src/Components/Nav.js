91806eb5f198b446cfbda11f97d7dad400b07357
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-12 16:11:00 +0530

    added appbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,13 +5,47 @@ import {
   Container,
   IconButton,
   Menu,
+  MenuItem,
   Toolbar,
   Tooltip,
-  Typography,
+  Typography
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AvatarImage from "../Assets/avatar.png";
+import { UserContext } from "../Contexts/UserContext";
 
 function Nav() {
+  const { userData, logout } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const settings = [
+    {
+      name: "Profile",
+      handler: () => {
+        setAnchorElUser(null);
+        navigate("/profile");
+      },
+    },
+    {
+      name: "Logout",
+      handler: () => {
+        setAnchorElUser(null);
+        logout();
+      },
+    },
+  ];
+
+  const handleOpenUserMenu = (e) => {
+    setAnchorElUser(e.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -24,7 +58,7 @@ function Nav() {
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
+              fontFamily: "cursive",
               fontWeight: 700,
               letterSpacing: ".3rem",
               color: "inherit",
@@ -34,42 +68,6 @@ function Nav() {
             Mentor Space
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              // onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              // anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              // open={Boolean(anchorElNav)}
-              // onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {/* {pages.map((page) => (
-              <MenuItem key={page} onClick={handleCloseNavMenu}>
-                <Typography textAlign="center">{page}</Typography>
-              </MenuItem>
-            ))} */}
-            </Menu>
-          </Box>
           <Typography
             variant="h5"
             noWrap
@@ -88,31 +86,20 @@ function Nav() {
           >
             LOGO
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {/* {pages.map((page) => (
-            <Button
-              key={page}
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              {page}
-            </Button>
-          ))} */}
-          </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
+          <Box sx={{ ml: "auto" }}>
             <Tooltip title="Open settings">
-              <IconButton
-                // onClick={handleOpenUserMenu}
-                sx={{ p: 0 }}
-              >
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  alt="Remy Sharp"
+                  src={userData?.profilePicture || AvatarImage}
+                />
               </IconButton>
             </Tooltip>
             <Menu
               sx={{ mt: "45px" }}
               id="menu-appbar"
-              //   anchorEl={anchorElUser}
+              anchorEl={anchorElUser}
               anchorOrigin={{
                 vertical: "top",
                 horizontal: "right",
@@ -122,14 +109,16 @@ function Nav() {
                 vertical: "top",
                 horizontal: "right",
               }}
-              //   open={Boolean(anchorElUser)}
-              //   onClose={handleCloseUserMenu}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
             >
-              {/* {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+              {settings.map((setting) => (
+                <MenuItem key={setting.name} onClick={handleCloseUserMenu}>
+                  <Typography variant="h6" onClick={setting.handler}>
+                    {setting.name}
+                  </Typography>
                 </MenuItem>
-              ))} */}
+              ))}
             </Menu>
           </Box>
         </Toolbar>
