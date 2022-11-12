e2f24765f5ae9ec6da202fcfe626be1fe1acd876
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-12 16:12:54 +0530

    feed page

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,10 +1,27 @@
+import { Grid } from "@mui/material";
+import { useSnackbar } from "notistack";
+import Left from "../Components/Feed/Left";
+import Mid from "../Components/Feed/Mid";
+import Right from "../Components/Feed/Right";
 import Nav from "../Components/Nav";
 
 function Feed() {
+  // eslint-disable-next-line no-unused-vars
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   return (
     <>
       <Nav />
-      feed
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          p: 1,
+        }}
+      >
+        <Left />
+        <Mid />
+        <Right />
+      </Grid>
     </>
   );
 }
