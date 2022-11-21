1e353526dc565ed13964b7055df9ec5076c5cc86
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-21 12:39:14 +0530

    refactored component with nav

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,24 +1,20 @@
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import Left from "../Components/Feed/Left";
 import Mid from "../Components/Feed/Mid";
-import Nav from "../Components/Nav";
 
 function Feed() {
   return (
-    <Box>
-      <Nav />
-      <Grid
-        container
-        spacing={2}
-        sx={{
-          p: 1,
-        }}
-        justifyContent="center"
-      >
-        <Left />
-        <Mid />
-      </Grid>
-    </Box>
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        p: 1,
+      }}
+      justifyContent="center"
+    >
+      <Left />
+      <Mid />
+    </Grid>
   );
 }
 
