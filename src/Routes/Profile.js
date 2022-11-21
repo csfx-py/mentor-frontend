7fbdec39fcb9303a32faba07e20166780b87d1cb
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-21 12:38:20 +0530

    Profile infor/edit page

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,5 +1,38 @@
+import { Grid, Paper } from "@mui/material";
+import ProfileAvatar from "../Components/Profile/ProfileAvatar";
+
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <Paper
+      sx={{
+        my: 2,
+        mx: 21,
+        p: 2,
+      }}
+      elevation={3}
+    >
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          p: 1,
+        }}
+        justifyContent="center"
+      >
+        <ProfileAvatar />
+        <Grid
+          item
+          xs={12}
+          md={8}
+          sx={{
+            p: 1,
+          }}
+        >
+          Profile Details
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
 
 export default Profile;
