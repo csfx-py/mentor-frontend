a059c90b7f8be64250465953004e39dc139f284d
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-25 10:12:37 +0530

    fixed get all posts to get current user and following tags post, added delete post request, fixed comments

--- src/Contexts/FeedContext.js
+++ src/Contexts/FeedContext.js
@@ -29,15 +29,67 @@ export const FeedProvider = ({ children, userData }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {}, [feedPosts]);
+  useEffect(() => {
+    setFeedPosts(null);
+  }, [userData]);
+
+  const getPosts = async (followingTags) => {
+    try {
+      setLoading(true);
+      const res = await API.get("/posts/get-all-posts", {
+        params: {
+          followingTags: JSON.stringify(followingTags),
+        },
+      });
+      if (res.data.success) {
+        setFeedPosts(res.data.posts || []);
+        setLoading(false);
+        return { success: true };
+      } else {
+        setLoading(false);
+        throw new Error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      setFeedPosts([]);
+      setLoading(false);
+      return {
+        success: false,
+        error: error.response?.data || error,
+      };
+    }
+  };
 
   const createPost = async (formData) => {
     try {
+      console.log(formData);
       setLoading(true);
       formData.append("user", userData._id);
       const res = await API.post("/posts/create", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
+        },
+      });
+      if (res.data.success) {
+        getPosts(userData?.followingTags || []);
+        setLoading(false);
+        return { success: true };
+      } else {
+        setLoading(false);
+        throw new Error(res.data.message);
+      }
+    } catch (error) {
+      setLoading(false);
+      return { success: false, error: error.response?.data || error };
+    }
+  };
+
+  const deletePost = async (postId) => {
+    try {
+      setLoading(true);
+      const res = await API.delete("/posts/delete", {
+        data: {
+          postId,
         },
       });
       if (res.data.success) {
@@ -79,40 +131,16 @@ export const FeedProvider = ({ children, userData }) => {
     }
   };
 
-  const getPosts = async (followingTags) => {
-    try {
-      setLoading(true);
-      const res = await API.get("/posts/get-all-posts", {
-        params: {
-          followingTags: JSON.stringify(followingTags),
-        },
-      });
-      if (res.data.success) {
-        setFeedPosts(res.data.posts);
-        setLoading(false);
-        return { success: true };
-      } else {
-        setLoading(false);
-        throw new Error(res.data.message);
-      }
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-      return {
-        success: false,
-        error: error.response?.data || error,
-        posts: [],
-      };
-    }
-  };
-
   return (
     <FeedContext.Provider
       value={{
         createPost,
+        deletePost,
         getPosts,
         addComment,
         feedPosts,
+        setFeedPosts,
+        tagOptions: tags,
       }}
     >
       {children}
