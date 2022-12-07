7ea0e56eb81b72cd453baa7d7fe2db44d7ce1116
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-07 23:51:28 +0530

    added post search bar to nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,25 +1,36 @@
+import { Clear, Search } from "@mui/icons-material";
 import {
   AppBar,
   Avatar,
   Box,
   Container,
   IconButton,
+  InputBase,
   Menu,
   MenuItem,
+  TextField,
   Toolbar,
   Tooltip,
-  Typography
+  Typography,
 } from "@mui/material";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AvatarImage from "../Assets/puneet_avatar.jpeg";
 import { UserContext } from "../Contexts/UserContext";
+import { FeedContext } from "../Contexts/FeedContext";
+import { useSnackbar } from "notistack";
 
 function Nav() {
+  // eslint-disable-next-line no-unused-vars
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
   const { userData, logout } = useContext(UserContext);
+  const { searchPosts } = useContext(FeedContext);
+
   const navigate = useNavigate();
 
   const [anchorElUser, setAnchorElUser] = useState(null);
+  const [query, setQuery] = useState("");
 
   const settings = [
     {
@@ -38,12 +49,30 @@ function Nav() {
     },
   ];
 
-  const handleOpenUserMenu = (e) => {
+  const handleOpenUserMenu = async (e) => {
     setAnchorElUser(e.currentTarget);
   };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
+  };
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    if (!query) {
+      enqueueSnackbar("Please enter a search query", { variant: "error" });
+      return;
+    }
+
+    const res = await searchPosts(query);
+
+    if (!res.success) {
+      enqueueSnackbar(res.error, { variant: "error" });
+      return;
+    }
+
+    navigate("/search");
   };
 
   return (
@@ -54,7 +83,7 @@ function Nav() {
         zIndex: 99,
       }}
     >
-      <Container maxWidth="xl">
+      <Container maxWidth="lg">
         <Toolbar disableGutters>
           <Typography
             variant="h6"
@@ -73,6 +102,47 @@ function Nav() {
           >
             Mentor Space
           </Typography>
+          <form
+            noValidate
+            autoComplete="off"
+            style={{
+              display: "flex",
+              flexGrow: 1,
+            }}
+            onSubmit={handleSearch}
+          >
+            <TextField
+              sx={{ ml: 1, flex: 1 }}
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              variant="standard"
+              size="small"
+              placeholder="Search Posts"
+              InputProps={{
+                sx: {
+                  color: "white",
+                  backgroundColor: "rgba(255, 255, 255, 0.1)",
+                  p: "10px",
+                  "&::placeholder": {
+                    color: "white",
+                  },
+                },
+              }}
+            />
+            <IconButton
+              type="submit"
+              sx={{
+                p: "10px",
+              }}
+              aria-label="search"
+            >
+              <Search
+                sx={{
+                  color: "white",
+                }}
+              />
+            </IconButton>
+          </form>
 
           <Typography
             variant="h5"
