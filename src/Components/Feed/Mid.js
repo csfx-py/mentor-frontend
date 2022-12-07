0129dc941aa6ec1da3ff46f2aa95410bbffbd6cd
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-07 23:52:02 +0530

    added feed layout without postform for searched posts

--- src/Components/Feed/Mid.js
+++ src/Components/Feed/Mid.js
@@ -5,20 +5,21 @@ import { UserContext } from "../../Contexts/UserContext";
 import Post from "./Post";
 import PostForm from "./PostForm";
 
-function Mid() {
+function Mid({ searched = false }) {
   const { userData } = useContext(UserContext);
   const { getPosts, feedPosts } = useContext(FeedContext);
 
   useEffect(() => {
-    getPosts(userData?.followingTags || [])
-      .then((res) => {
-        if (!res.success) {
-          console.log(res.error);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (!searched)
+      getPosts(userData?.followingTags || [])
+        .then((res) => {
+          if (!res.success) {
+            console.log(res.error);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userData?.followingTags]);
 
@@ -26,14 +27,14 @@ function Mid() {
     <Grid
       item
       xs={12}
-      md={7}
+      md={8}
       sx={{
         height: "calc(100vh - 64px)",
         overflowY: "auto",
       }}
     >
       <Grid container direction={"column"}>
-        <PostForm />
+        {!searched && <PostForm />}
         {feedPosts &&
           feedPosts.map((post) => <Post key={post._id} post={post} />)}
       </Grid>
