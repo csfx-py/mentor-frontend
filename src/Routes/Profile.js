aa8d37567220cc869649f41a577206035670c9ff
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-10 13:02:01 +0530

    profile route profile details, form and own posts

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,37 +1,65 @@
-import { Grid, Paper } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
+import { useContext, useEffect } from "react";
+import Mid from "../Components/Feed/Mid";
+import ProfielDetails from "../Components/Profile/ProfielDetails";
 import ProfileAvatar from "../Components/Profile/ProfileAvatar";
+import { FeedContext } from "../Contexts/FeedContext";
+import { LoadingContext } from "../Contexts/LoadingContext";
+import { UserContext } from "../Contexts/UserContext";
 
 function Profile() {
+  const { getMyPosts } = useContext(FeedContext);
+  const { setLoading } = useContext(LoadingContext);
+  const { userData } = useContext(UserContext);
+
+  useEffect(() => {
+    getMyPosts().then((res) => {
+      if (!res.success) {
+        console.log(res.error);
+      }
+      setLoading(false);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userData]);
+
   return (
-    <Paper
-      sx={{
-        my: 2,
-        mx: 21,
-        p: 2,
-      }}
-      elevation={3}
-    >
-      <Grid
-        container
-        spacing={2}
+    <>
+      <Paper
         sx={{
-          p: 1,
+          my: 2,
+          mx: 21,
+          p: 2,
         }}
-        justifyContent="center"
+        elevation={3}
       >
-        <ProfileAvatar />
         <Grid
-          item
-          xs={12}
-          md={8}
+          container
+          spacing={2}
           sx={{
             p: 1,
           }}
+          justifyContent="center"
         >
-          Profile Details
+          <ProfileAvatar />
+          <ProfielDetails />
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+      <Paper elevation={3} sx={{ my: 2, mx: 21, p: 2 }}>
+        <Typography variant="h5" sx={{ mx: 21, my: 2 }} align="center">
+          Your Posts
+        </Typography>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            p: 1,
+          }}
+          justifyContent="center"
+        >
+          <Mid searched={true} style={{ height: "100%" }} />
+        </Grid>
+      </Paper>
+    </>
   );
 }
 
