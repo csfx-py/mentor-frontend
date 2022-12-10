f5b452a286afbafb2f4a2328122eaa715fbc69df
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-10 17:57:06 +0530

    fixed loading overflow and smooth scroll

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { useContext } from "react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import ComponentWithNav from "./Components/ComponentWithNav";
-import Err404 from "./Routes/Err404";
 import Loading from "./Components/Loading";
 import { FeedProvider } from "./Contexts/FeedContext";
 import { LoadingContext } from "./Contexts/LoadingContext";
 import { UserContext } from "./Contexts/UserContext";
 import Auth from "./Routes/Auth";
-import Landing from "./Routes/Landing";
+import Err404 from "./Routes/Err404";
 import Feed from "./Routes/Feed";
+import Landing from "./Routes/Landing";
 import Profile from "./Routes/Profile";
 import Search from "./Routes/Search";
 import { mentorTheme } from "./Theme";
@@ -22,62 +22,70 @@ function App() {
   const { pathname, state } = useLocation();
 
   return (
-    <ThemeProvider theme={mentorTheme}>
-      <div className="App">
-        {loading && <Loading />}
-        <FeedProvider userData={userData}>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                user ? <Navigate to="/feed" state={pathname} /> : <Landing />
-              }
-            />
-            <Route
-              path="/auth"
-              element={user ? <Navigate to={state} /> : <Auth />}
-            />
-            <Route
-              path="/feed"
-              element={
-                user ? (
-                  <ComponentWithNav>
-                    <Feed />
-                  </ComponentWithNav>
-                ) : (
-                  <Navigate to="/auth" state={pathname} />
-                )
-              }
-            />
-            <Route
-              path="/search"
-              element={
-                user ? (
-                  <ComponentWithNav>
-                    <Search />
-                  </ComponentWithNav>
-                ) : (
-                  <Navigate to="/auth" state={pathname} />
-                )
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                user ? (
-                  <ComponentWithNav>
-                    <Profile />
-                  </ComponentWithNav>
-                ) : (
-                  <Navigate to="/auth" state={pathname} />
-                )
-              }
-            />
-            <Route path="*" element={<Err404 />} />
-          </Routes>
-        </FeedProvider>
+    <>
+      {loading && <Loading />}
+      <div
+        className="App"
+        style={{
+          overflow: loading ? "hidden" : "auto",
+          height: loading ? "100vh" : "auto",
+        }}
+      >
+        <ThemeProvider theme={mentorTheme}>
+          <FeedProvider userData={userData}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  user ? <Navigate to="/feed" state={pathname} /> : <Landing />
+                }
+              />
+              <Route
+                path="/auth"
+                element={user ? <Navigate to={state} /> : <Auth />}
+              />
+              <Route
+                path="/feed"
+                element={
+                  user ? (
+                    <ComponentWithNav>
+                      <Feed />
+                    </ComponentWithNav>
+                  ) : (
+                    <Navigate to="/auth" state={pathname} />
+                  )
+                }
+              />
+              <Route
+                path="/search"
+                element={
+                  user ? (
+                    <ComponentWithNav>
+                      <Search />
+                    </ComponentWithNav>
+                  ) : (
+                    <Navigate to="/auth" state={pathname} />
+                  )
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  user ? (
+                    <ComponentWithNav>
+                      <Profile />
+                    </ComponentWithNav>
+                  ) : (
+                    <Navigate to="/auth" state={pathname} />
+                  )
+                }
+              />
+              <Route path="*" element={<Err404 />} />
+            </Routes>
+          </FeedProvider>
+        </ThemeProvider>
       </div>
-    </ThemeProvider>
+    </>
   );
 }
 
