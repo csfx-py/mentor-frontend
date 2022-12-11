904d6eb65445b6b7ed6f5fc6901b272c0b0dfa2e
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-11 23:53:57 +0530

    mapped routes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import ComponentWithNav from "./Components/ComponentWithNav";
 import Loading from "./Components/Loading";
+import { AdminProvider } from "./Contexts/AdminContext";
 import { FeedProvider } from "./Contexts/FeedContext";
 import { LoadingContext } from "./Contexts/LoadingContext";
 import { UserContext } from "./Contexts/UserContext";
+import AdminsTags from "./Routes/AdminsTags";
 import Auth from "./Routes/Auth";
 import Err404 from "./Routes/Err404";
 import Feed from "./Routes/Feed";
@@ -17,7 +19,7 @@ import { mentorTheme } from "./Theme";
 
 function App() {
   const { loading } = useContext(LoadingContext);
-  const { user, userData } = useContext(UserContext);
+  const { user, role, userData } = useContext(UserContext);
 
   const { pathname, state } = useLocation();
 
@@ -33,55 +35,106 @@ function App() {
       >
         <ThemeProvider theme={mentorTheme}>
           <FeedProvider userData={userData}>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  user ? <Navigate to="/feed" state={pathname} /> : <Landing />
-                }
-              />
-              <Route
-                path="/auth"
-                element={user ? <Navigate to={state} /> : <Auth />}
-              />
-              <Route
-                path="/feed"
-                element={
-                  user ? (
-                    <ComponentWithNav>
-                      <Feed />
-                    </ComponentWithNav>
-                  ) : (
-                    <Navigate to="/auth" state={pathname} />
-                  )
-                }
-              />
-              <Route
-                path="/search"
-                element={
-                  user ? (
-                    <ComponentWithNav>
-                      <Search />
-                    </ComponentWithNav>
-                  ) : (
-                    <Navigate to="/auth" state={pathname} />
-                  )
-                }
-              />
-              <Route
-                path="/profile"
-                element={
-                  user ? (
-                    <ComponentWithNav>
-                      <Profile />
-                    </ComponentWithNav>
-                  ) : (
-                    <Navigate to="/auth" state={pathname} />
-                  )
-                }
-              />
-              <Route path="*" element={<Err404 />} />
-            </Routes>
+            <AdminProvider userData={userData}>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    user ? (
+                      <Navigate to="/feed" state={pathname} />
+                    ) : (
+                      <Landing />
+                    )
+                  }
+                />
+                <Route
+                  path="/auth"
+                  element={user ? <Navigate to={state} /> : <Auth />}
+                />
+                <Route
+                  path="/feed"
+                  element={
+                    user ? (
+                      <ComponentWithNav>
+                        <Feed />
+                      </ComponentWithNav>
+                    ) : (
+                      <Navigate to="/auth" state={pathname} />
+                    )
+                  }
+                />
+                <Route
+                  path="/search"
+                  element={
+                    user ? (
+                      <ComponentWithNav>
+                        <Search />
+                      </ComponentWithNav>
+                    ) : (
+                      <Navigate to="/auth" state={pathname} />
+                    )
+                  }
+                />
+                <Route
+                  path="/profile"
+                  element={
+                    user ? (
+                      <ComponentWithNav>
+                        <Profile />
+                      </ComponentWithNav>
+                    ) : (
+                      <Navigate to="/auth" state={pathname} />
+                    )
+                  }
+                />
+                <Route
+                  path="/admin/posts"
+                  element={
+                    role === "admin" ? (
+                      <ComponentWithNav>
+                        <>Admin Posts</>
+                      </ComponentWithNav>
+                    ) : (
+                      <Navigate to="/404" state={pathname} />
+                    )
+                  }
+                />
+                <Route
+                  path="/admin/tags"
+                  element={
+                    role === "admin" ? (
+                      <ComponentWithNav>
+                        <AdminsTags />
+                      </ComponentWithNav>
+                    ) : (
+                      <Navigate to="/404" state={pathname} />
+                    )
+                  }
+                />
+                <Route
+                  path="/admin/users"
+                  element={
+                    role === "admin" ? (
+                      <ComponentWithNav>
+                        <>Admin Users</>
+                      </ComponentWithNav>
+                    ) : (
+                      <Navigate to="/404" state={pathname} />
+                    )
+                  }
+                />
+                <Route
+                  path="*"
+                  element={
+                    state?.startsWith("/admin") && role === "admin" ? (
+                      <Navigate to={state} />
+                    ) : (
+                      <Err404 />
+                    )
+                  }
+                />
+              </Routes>
+            </AdminProvider>
           </FeedProvider>
         </ThemeProvider>
       </div>
