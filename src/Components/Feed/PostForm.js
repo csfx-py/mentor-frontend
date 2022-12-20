0ea914aeaa43766bb7031fbf98c8991a368b1857
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-20 14:54:21 +0530

    changed post form to implement paid posts

--- src/Components/Feed/PostForm.js
+++ src/Components/Feed/PostForm.js
@@ -1,10 +1,9 @@
-import { Delete } from "@mui/icons-material";
+import { Delete, Paid } from "@mui/icons-material";
 import {
   Autocomplete,
   Button,
   Grid,
   IconButton,
-  List,
   Paper,
   TextField,
   Typography,
@@ -21,7 +20,10 @@ const draggedStyle = {
 
 function PostForm() {
   const [files, setFiles] = useState([]);
+  const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [isPaid, setIsPaid] = useState(false);
+  const [price, setPrice] = useState(0);
   const [tags, setTags] = useState([]);
   const [dragging, setDragging] = useState(false);
 
@@ -57,20 +59,56 @@ function PostForm() {
   };
 
   const handleTextChange = (e) => {
-    setDescription(e.target.value);
+    switch (e.target.name) {
+      case "title":
+        setTitle(e.target.value);
+        break;
+      case "description":
+        setDescription(e.target.value);
+        break;
+      case "price":
+        setPrice(e.target.value);
+        break;
+      default:
+        break;
+    }
   };
 
   const validate = () => {
-    if (description?.length === 0 || tags?.length === 0) {
-      enqueueSnackbar(
-        "Please add some description with tags and optionally some files ",
-        {
-          variant: "info",
-        }
-      );
-      return false;
+    if (
+      !(
+        description?.length === 0 ||
+        tags?.length === 0 ||
+        title?.length === 0 ||
+        (isPaid && price === 0)
+      )
+    )
+      return true;
+    switch (true) {
+      case description?.length === 0:
+        enqueueSnackbar("Description is required", {
+          variant: "error",
+        });
+        break;
+      case title?.length === 0:
+        enqueueSnackbar("Title is required", {
+          variant: "error",
+        });
+        break;
+      case tags?.length === 0:
+        enqueueSnackbar("Tags are required", {
+          variant: "error",
+        });
+        break;
+      case isPaid && price === 0:
+        enqueueSnackbar("Price is required", {
+          variant: "error",
+        });
+        break;
+      default:
+        break;
     }
-    return true;
+    return false;
   };
 
   const handleSubmit = async (e) => {
@@ -84,7 +122,10 @@ function PostForm() {
 
     const formData = new FormData();
 
+    formData.append("title", title);
     formData.append("description", description);
+    formData.append("isPaid", isPaid);
+    formData.append("price", price);
 
     tags.forEach((tag) => {
       formData.append("tags", tag._id);
@@ -101,6 +142,9 @@ function PostForm() {
         variant: "success",
       });
       setFiles([]);
+      setTitle("");
+      setIsPaid(false);
+      setPrice(0);
       setDescription("");
       setTags([]);
     } else {
@@ -120,8 +164,64 @@ function PostForm() {
       >
         <Typography variant="h5">Create Post</Typography>
         <form onDragEnter={handleDrag} onSubmit={handleSubmit}>
+          <Grid container spacing={1}>
+            <Grid item xs={6}>
+              <TextField
+                name="title"
+                label="Document title"
+                size="small"
+                fullWidth
+                variant="outlined"
+                value={title}
+                onChange={handleTextChange}
+                sx={{
+                  mb: 1,
+                }}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              Is the post paid?
+              <IconButton
+                variant="text"
+                onClick={() => setIsPaid(!isPaid)}
+                size="large"
+                sx={{
+                  borderRadius: 5,
+                }}
+              >
+                {isPaid ? (
+                  <>
+                    <Paid color="success" />
+                    Yes
+                  </>
+                ) : (
+                  <>
+                    <Paid color="error" />
+                    No
+                  </>
+                )}
+              </IconButton>
+            </Grid>
+            {isPaid && (
+              <Grid item xs={3}>
+                <TextField
+                  name="price"
+                  type="number"
+                  label="Price"
+                  size="small"
+                  fullWidth
+                  variant="outlined"
+                  value={price}
+                  onChange={handleTextChange}
+                  sx={{
+                    mb: 1,
+                  }}
+                />
+              </Grid>
+            )}
+          </Grid>
           <TextField
-            id="outlined-multiline-static"
+            name="description"
             label="Document description"
             multiline
             rows={4}
@@ -134,49 +234,95 @@ function PostForm() {
             }}
           />
           {/* file input with drag and drop */}
-          <div
-            onDragEnter={handleDrag}
-            onDragOver={handleDrag}
-            onDragLeave={handleDrag}
-            onDrop={handleDrop}
-            style={dragging ? draggedStyle : {}}
-          >
-            <input
-              type="file"
-              id="file"
-              multiple
-              onChange={handleFileChange}
-              style={{ display: "none" }}
-            />
-            <label htmlFor="file">
-              <Button
-                variant="raised"
-                component="span"
-                fullWidth
-                sx={{
-                  p: 1,
-                  py: 3,
-                  border: "2px dashed #ccc",
-
-                  "&:focus": {
-                    border: "2px dashed #000",
-                    backgroundColor: "rgba(0,0,0,.05)",
-                  },
-                }}
+          <Grid container spacing={1}>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              sx={{
+                pt: "16px !important",
+              }}
+            >
+              <div
+                onDragEnter={handleDrag}
+                onDragOver={handleDrag}
+                onDragLeave={handleDrag}
+                onDrop={handleDrop}
+                style={dragging ? draggedStyle : {}}
               >
-                Upload Files
-              </Button>
-            </label>
-          </div>
+                <input
+                  type="file"
+                  id="file"
+                  multiple
+                  onChange={handleFileChange}
+                  style={{ display: "none" }}
+                />
+                <label htmlFor="file">
+                  <Button
+                    variant="raised"
+                    component="span"
+                    fullWidth
+                    sx={{
+                      p: 1,
+                      pt: 2,
+                      pb: 3,
+                      border: "2px dashed #ccc",
+
+                      "&:focus": {
+                        border: "2px dashed #000",
+                        backgroundColor: "rgba(0,0,0,.05)",
+                      },
+                    }}
+                  >
+                    Upload Files
+                  </Button>
+                </label>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Autocomplete
+                sx={{
+                  mt: 1,
+                }}
+                multiple
+                id="tags-outlined"
+                options={tagOptions || []}
+                getOptionLabel={(option) => option.name}
+                filterSelectedOptions
+                renderInput={(params) => (
+                  <TextField {...params} label="Tags" placeholder="Tags" />
+                )}
+                value={tags}
+                onChange={(e, value) => {
+                  setTags(value);
+                }}
+              />
+            </Grid>
+          </Grid>
           {files.length > 0 && (
-            <List
+            <Grid
+              container
+              spacing={1}
               sx={{
                 p: 0,
                 mt: 1,
               }}
             >
               {files.map((file) => (
-                <div key={file.name}>
+                <Grid
+                  item
+                  key={file.name}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    p: 2,
+                    border: "1px solid #ccc",
+                    borderRadius: 10,
+                    backgroundColor: "rgba(0,0,0,.05)",
+                    m: 1,
+                  }}
+                >
                   {file.name}
                   <IconButton
                     size="small"
@@ -190,27 +336,10 @@ function PostForm() {
                       }}
                     />
                   </IconButton>
-                </div>
+                </Grid>
               ))}
-            </List>
+            </Grid>
           )}
-          <Autocomplete
-            sx={{
-              mt: 1,
-            }}
-            multiple
-            id="tags-outlined"
-            options={tagOptions || []}
-            getOptionLabel={(option) => option.name}
-            filterSelectedOptions
-            renderInput={(params) => (
-              <TextField {...params} label="Tags" placeholder="Tags" />
-            )}
-            value={tags}
-            onChange={(e, value) => {
-              setTags(value);
-            }}
-          />
           <Button variant="contained" type="submit" fullWidth sx={{ mt: 1 }}>
             Submit
           </Button>
