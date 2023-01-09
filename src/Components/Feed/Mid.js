ee63369fcd7920d33f539af4623bed6dddd5f174
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-09 12:06:19 +0530

    showing user profile and follow button

--- src/Components/Feed/Mid.js
+++ src/Components/Feed/Mid.js
@@ -1,14 +1,34 @@
 import { SentimentVeryDissatisfied } from "@mui/icons-material";
-import { Grid, Typography } from "@mui/material";
-import { useContext, useEffect } from "react";
+import { Avatar, Button, Grid, Typography } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
 import { FeedContext } from "../../Contexts/FeedContext";
 import { UserContext } from "../../Contexts/UserContext";
+import AvatarImage from "../../Assets/puneet_avatar.jpeg";
 import Post from "./Post";
 import PostForm from "./PostForm";
+import { useSnackbar } from "notistack";
 
-function Mid({ searched = false, style = {}, allPosts = null }) {
-  const { userData } = useContext(UserContext);
+function Mid({
+  searched = false,
+  style = {},
+  allPosts = null,
+  searchedUser = null,
+}) {
+  const { userData, followOrUnfollow } = useContext(UserContext);
   const { getPosts, feedPosts } = useContext(FeedContext);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [following, setFollowing] = useState(false);
+
+  useEffect(() => {
+    if (searchedUser) {
+      console.log(userData);
+      setFollowing(
+        userData?.following?.find((user) => user._id === searchedUser._id)
+      );
+    }
+  }, [searchedUser, userData]);
 
   useEffect(() => {
     if (!searched)
@@ -36,6 +56,50 @@ function Mid({ searched = false, style = {}, allPosts = null }) {
         ...style,
       }}
     >
+      {searchedUser && (
+        <Grid container direction={"column"} sx={{ p: 2 }}>
+          <Grid item xs={6} sx={{ mx: "auto" }}>
+            <Avatar
+              src={searchedUser?.profilePic || AvatarImage}
+              sx={{
+                width: 100,
+                height: 100,
+                mx: "auto",
+                mt: 2,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6} sx={{ mx: "auto" }}>
+            <Typography variant="h5">{searchedUser?.name}</Typography>
+          </Grid>
+          <Grid item xs={6} sx={{ mx: "auto" }}>
+            <Typography variant="caption">
+              {searchedUser?.posts?.length} Posts
+            </Typography>
+          </Grid>
+          {searchedUser?._id !== userData?._id && (
+            <Grid item xs={6} sx={{ mx: "auto" }}>
+              <Button
+                variant="contained"
+                sx={{ mt: 2 }}
+                onClick={async (e) => {
+                  e.preventDefault();
+                  const res = await followOrUnfollow(searchedUser?._id);
+                  if (!res.success) {
+                    enqueueSnackbar(res?.error?.message, { variant: "error" });
+                    return;
+                  }
+                  enqueueSnackbar("User Followed", { variant: "success" });
+                }}
+              >
+                {userData?.following?.includes(searchedUser?._id)
+                  ? "Unfollow"
+                  : "Follow"}
+              </Button>
+            </Grid>
+          )}
+        </Grid>
+      )}
       <Grid container direction={"column"} sx={{ p: 2 }}>
         {!searched && <PostForm />}
         {allPosts &&
